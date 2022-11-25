80b45e5628310f669b92612e2c36899267ba7484
Marek Lassak <mlassak@redhat.com>
2022-11-26 00:18:12 +0100

    init app setup with firebase auth and db

---
+++ src/components/Layout.tsx
@@ -0,0 +1,49 @@
+import { FC, PropsWithChildren } from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Container, Toolbar, Button, Box } from '@mui/material';
+
+import useLoggedInUser from '../hooks/useLoggedInUser';
+import { signOut } from '../utils/firebase';
+
+const Layout: FC<PropsWithChildren> = ({ children }) => {
+	const user = useLoggedInUser();
+
+	return (
+		<>
+			<AppBar sx={{ position: 'sticky', top: 0 }}>
+				<Container maxWidth="sm">
+					<Toolbar disableGutters sx={{ gap: 2 }}>
+						<Button component={Link} to="/">
+							Home
+						</Button>
+						<Box sx={{ flexGrow: 1 }} />
+						{!user ? (
+							<Button component={Link} to="/login">
+								Login
+							</Button>
+						) : (
+							<Button onClick={signOut}>Logout</Button>
+						)}
+					</Toolbar>
+				</Container>
+			</AppBar>
+
+			<Container
+				maxWidth="sm"
+				component="main"
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexGrow: 1,
+					gap: 2,
+					py: 2
+				}}
+			>
+				{children}
+			</Container>
+		</>
+	);
+};
+export default Layout;
---
+++ src/utils/firebase.ts
@@ -0,0 +1,36 @@
+import { initializeApp } from 'firebase/app';
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut as authSignOut,
+	onAuthStateChanged,
+	User
+} from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+import { firebaseConfig } from './firebase-config';
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Authentication
+const auth = getAuth();
+
+// Sign up handler
+export const signUp = (email: string, password: string) =>
+	createUserWithEmailAndPassword(auth, email, password);
+
+// Sign in handler
+export const signIn = (email: string, password: string) =>
+	signInWithEmailAndPassword(auth, email, password);
+
+// Sign out handler
+export const signOut = () => authSignOut(auth);
+
+// Subscribe to auth state changes
+export const onAuthChanged = (callback: (u: User | null) => void) =>
+	onAuthStateChanged(auth, callback);
+
+// Firestore
+const _ = getFirestore(); // db not used yet
